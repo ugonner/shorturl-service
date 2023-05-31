5149f3ce32d82d018876f082d8ba4caadb48e663
ugonner <umeokwonnabonaventure@gmail.com>
2023-05-31 13:17:18 +0100

    Sets project structure, adds url folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UrlModule } from './url/url.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
+    UrlModule,
   ],
   controllers: [AppController],
   providers: [AppService],
