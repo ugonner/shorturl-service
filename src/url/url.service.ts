f699bc2b17c984267529c0667163f120c31b9fbb
ugonner <umeokwonnabonaventure@gmail.com>
2023-06-01 07:43:33 +0100

    adds additional verification of url dns

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -5,7 +5,9 @@ import { URLStore, URLStoreDocument } from './url.schema';
 import { ApiResponse, IGenericResponse } from 'src/shared/apiResponse';
 import { EncodeURLDto } from './dtos/url.dto';
 import { IURLData } from 'src/shared/typings';
-
+import {URL} from 'url';
+import * as http from 'https';
+import * as dns from 'dns'
 @Injectable()
 export class UrlService {
     constructor(
@@ -28,18 +30,39 @@ export class UrlService {
 
     async encodeURL(url: string): Promise<IGenericResponse<IURLData | unknown>>{
         try{
+            const isVerifiedUrl = (await UrlService.isWorkingUrl(url));
+            console.log("error", isVerifiedUrl);
+            if(!isVerifiedUrl.status) return ApiResponse.fail("This is not a working URL, check your server information", HttpStatus.UNPROCESSABLE_ENTITY);
+
             const urlExists = await this.urlStoreModel.findOne({originalUrl: url});
-            if(urlExists) return ApiResponse.fail("This URL is already encoded", HttpStatus.UNPROCESSABLE_ENTITY);
+            if(urlExists) return ApiResponse.fail("This URL is not in our system", HttpStatus.UNPROCESSABLE_ENTITY);
             const shortCodeResult = await this.getUrlStringUniqueCode();
             if(!shortCodeResult.status) return shortCodeResult;
             await this.urlStoreModel.create({
                 originalUrl: url,
                 shortCode: shortCodeResult.data
             });
-            return ApiResponse.success("url encoded successfully", HttpStatus.OK, {originalUrl: url, shortUrl: `${process.env.BASE_URL + shortCodeResult.data}`});
+            return ApiResponse.success("url encoded successfully", HttpStatus.OK, {originalUrl: url, shortUrl: `${process.env.SHORT_BASE_URL + shortCodeResult.data}`});
         }catch(error){
             return ApiResponse.fail(error.message, HttpStatus.INTERNAL_SERVER_ERROR, error);
         }
     }
+
+static async isWorkingUrl(url: string): Promise<IGenericResponse<boolean>>{
+  try {
+    url = new URL(url).hostname;
+    const requestURLPromise = new Promise((resolve, reject) => {
+      const req = dns.lookup(url, (error, data) => {
+        error ? reject(error) : resolve(data);
+      });
+    });
+
+    const validResponse = await requestURLPromise;
+    return validResponse ? ApiResponse.success("valid", HttpStatus.OK, true): ApiResponse.fail("invalid", HttpStatus.OK, validResponse);
+
     
+  } catch (error) {
+    return ApiResponse.fail(error.message, HttpStatus.INTERNAL_SERVER_ERROR, error); // Resolve with false if the URL is invalid
+  }
+}
 }
